50c673c55c7cefdc282a239646448680dd053f53
arvidsigvardsson <arvid.sigvardsson@malmo.se>
2017-04-20 17:09:04 +0200

    Initial commit

---
+++ SBKAvtalskatalog/Controllers/KatalogController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBKAvtalskatalog.Controllers
+{
+    [RoutePrefix("Arvid")]
+    [Route("{action=index}")]
+    public class KatalogController : Controller
+    {
+        // GET: Katalog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Start")]
+        public ActionResult Start()
+        {
+            return View();
+        }
+
+        public ActionResult TestPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ SBKAvtalskatalog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBKAvtalskatalog.MvcApplication" Language="C#" %>
