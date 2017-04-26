4e7bc2164e27500e6197f096df4ebcd9ae1ecb1f
arvidsigvardsson <arvid.sigvardsson@malmo.se>
2017-04-26 12:52:10 +0200

    La till sql fil och lite till

---
+++ postgresql/tablecreator.sql
@@ -0,0 +1,13 @@
+﻿drop table if exists avtal;
+create table avtal(
+	id			serial primary key,
+	diarienummer		bigint not null,
+	startdate		date,
+	enddate			date,
+	orgnummer		varchar(20)
+);
+
+insert into avtal(diarienummer, startdate, enddate, orgnummer)
+values(314, '2017-01-01', CURRENT_DATE, '820403');
+
+select * from avtal;
